288e27e756b92e6dc9eefa8832f4b9f945bfb470
EVOAUCTION <125301054+JNDUNLAP@users.noreply.github.com>
2023-12-03 19:05:20 -0500

    big push

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+client_id = os.getenv("YOUR_CLIENT_ID")
+client_secret = os.getenv("YOUR_CLIENT_SECRET")
+
+# Get Emails, or Folder, or Excel Files
